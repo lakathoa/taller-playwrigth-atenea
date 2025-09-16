2177e85892ef7c6c2fa43c76a296e2c76adcb1e2
lady.orozco <lady.orozco@intraway.com>
2025-09-16 14:03:11 -0500

    Se agrega backendUtilst a  escenario de BackEnd

---
+++ tests/loginBE.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect, request } from '@playwright/test';
+import { LoginPage } from '../pages/loginPage';
+import { DashboardPage } from '../pages/dashboardPage';
+import TestData from '../data/testData.json'
+import { BackendUtils } from '../utils/backendUtils';
+
+let loginPage: LoginPage; //Toda la clase va a tener acceso a esta variable
+let dashboardPage: DashboardPage;
+
+test.beforeEach(async ({ page}) => {
+  loginPage = new LoginPage(page);
+  dashboardPage = new DashboardPage(page);
+  await loginPage.visitarPaginaLogin();
+})
+
+test('TC-19 Loguearse con un nuevo usuario creado por BE y validar response', async ({ page, request }) => {
+  const nuevoUsuario = await BackendUtils.crearUsuarioPorAPI(request, TestData.usuarioValido)
+  const responsePromiseLogin = page.waitForResponse('http://localhost:4000/api/auth/login');
+  await loginPage.completarYHacerClickBotonLogin(nuevoUsuario);
+  
+  const responseLogin = await responsePromiseLogin;//esperando la respuesta del request de la línea 58
+  const responseBodyLoginJson = await responseLogin.json();//guardamos la respuesta del request
+
+  expect(responseLogin.status()).toBe(200);
+  expect(responseBodyLoginJson).toHaveProperty('token');//en esta valida que exista como tal la propiedad token
+  expect(typeof responseBodyLoginJson.token).toBe('string');//nos valida que el valos de la llave token sea un string
+  expect(responseBodyLoginJson).toHaveProperty('user');
+  expect(responseBodyLoginJson.user).toEqual(expect.objectContaining({ //Va a validar lo que respondió con lo que tenemos en nuestro archivo de TestData
+      id: expect.any(String),
+      firstName: TestData.usuarioValido.nombre,
+      lastName: TestData.usuarioValido.apellido,
+      email: nuevoUsuario.email
+    }));
+
+  await expect(page.getByText('Inicio de sesión exitoso')).toBeVisible();
+  await expect(dashboardPage.dashboardTitle).toBeVisible();
+});
--- utils/backendUtils.ts
+++ utils/backendUtils.ts
@@ -1,2 +1,22 @@
-import { test, expect, request } from '@playwright/test';
+import { APIRequestContext, expect} from '@playwright/test';
+
+export class BackendUtils{
+    static async crearUsuarioPorAPI(request: APIRequestContext, usuario: any){
+        const email = (usuario.email.split('@')[0]) + Date.now().toString() + '@' + (usuario.email.split('@')[1]);
+        const response = await request.post('http://localhost:4000/api/auth/signup',{
+        headers: {
+            'Accept': 'application/vnd.github.v3+json',
+            'content-type': 'application/json'
+        },
+        data: {
+            firstName: usuario.nombre,
+            lastName: usuario.apellido,
+            email: email,
+            password: usuario.contraseña
+        }
+    });
+    expect(response.status()).toBe(201);
+    return{email: email, contraseña: usuario.contraseña}
+    }
+}
 
